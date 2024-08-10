85792dd267a4ada5475ccc006044418c41b8da41
Rufat <mrufat755@gmail.com>
2024-08-10 15:03:58 +0400

    finished-selected-genre-page

--- src/Pages/loginRegister/SelectedGenre/SelectedGenre.tsx
+++ src/Pages/loginRegister/SelectedGenre/SelectedGenre.tsx
@@ -2,10 +2,13 @@ import "./SelectedGenre.scss";
 import { useMyContext } from "../../../UseContext";
 import { Movie } from "../../../UserInterface";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
 
 const SelectedGenre = () => {
-  const { selectedGenre, moviesBase } = useMyContext();
+  const { selectedGenre, moviesBase,setSelectedGenre,setDescriptionData,descriptionData } = useMyContext();
   const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
+  const navigate = useNavigate(); 
 
   useEffect(() => {
     const updatedFilterMovies = moviesBase.filter((item) =>
@@ -14,26 +17,58 @@ const SelectedGenre = () => {
     setFilteredMovies(updatedFilterMovies);
   }, [selectedGenre, moviesBase]);
 
+
+  const getMovieGenre = (e:any) =>{
+    setSelectedGenre(e.target.textContent)
+  }
+  //к слову эти две функции та что сверху и та что снизу используется в двух местах но по скольку
+  // они были маленькими я не стал переиспользовать их я знаю что это неправильно но меня время поджимает
+  // а как это сделать правильно надо было эти две функции написать в UseContext и использовать те же стейты и
+  //в качестве параметров передавать нужные нам данные 
+
+  const getItemToDescription = (itemId:string) =>{
+    const movieToDescription = moviesBase.find((item)=>item.id ===itemId);
+    setDescriptionData(movieToDescription);
+    navigate('/Description'); 
+  }
+
   return (
-    <section className="selected-genre container">
-      <div className="selected-genre-title">Films in the selected genre</div>
-      {filteredMovies.map((item) => (
-        <div className="selectet-movies">
-          <div className="movie-poster">
-            <img src={item.image} />
-          </div>
-          <div className="movie-description">
-            <p className="movie-title">{item.title}</p>
-            <div className="movies-rank-imbdb">
-              <p className="rank">{item.rank}</p>
-              <p className="imdb">{item.rating}</p>
-            </div>
-          </div>
+    <section className="selected-genre-section">
+      <div className="selected-genre-section-inner container">
+        <div className="selected-genre-section-title">
+          <h1 className="selected-genre-section-title-text">Selected genres</h1>
         </div>
-      ))}
+
+        <div className="selected-genre-items">
+          {filteredMovies.map((item) => (
+            <div className="selected-genre-item" key={item.id}>
+              <button className="selected-genre-item-img" key={item.id} onClick={()=>getItemToDescription(item.id)}>
+                <img className="selected-genre-item-poster" src={item.image} />
+              </button>
+              <div className="selected-genre-item-desc">
+                <h4 className="selected-genre-item-desc-title">{item.title}</h4>
+                <ul className="selected-genre-item-desc-jenres-items">
+                  {item.genre.map((item) => (
+                    <button className="selected-genre-item-desc-jenres-item" onClick={(e)=>getMovieGenre(e)}>
+                      {item}
+                    </button>
+                  ))}
+                  <div className="selected-genre-item-desc-jenres-item-links">
+                    <a
+                      href={item.imdb_link}
+                      className="selected-genre-item-desc-jenres-item-link"
+                    >
+                      Watch
+                    </a>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
 
 export default SelectedGenre;
-
