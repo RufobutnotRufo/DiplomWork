d6de6ac9d270555ae02dced59e0d013f0ed666e5
Rufat <mrufat755@gmail.com>
2024-08-10 21:33:25 +0400

    add-search-func

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,18 +3,35 @@ import BusketSvg from "../assets/busket-svg.svg";
 import "./Header.scss";
 import { useMyContext } from "../UseContext";
 import { useState } from "react";
+import { Movie } from "../UserInterface";
 
 const Header: React.FC = () => {
-  const { userData } = useMyContext();
-  const [seacrhMenuToggle,setSeacrhMenuToggle] = useState(false);
+  const { userData, moviesBase } = useMyContext();
+  const [seacrhMenuToggle, setSeacrhMenuToggle] = useState<boolean>(false);
+  const [seacrhInputValue, setSearchInputValue] = useState<string>("");
+  const [seacrhetDatas, setSeacrhetDatas] = useState<Movie[]>([]);
 
+  const searchModalTogleOpen = () => {
+    setSeacrhMenuToggle(true);
+  };
+  const searchModalTogleClose = () => {
+    setSeacrhMenuToggle(false);
+  };
 
-  const searchModalTogleOpen = () =>{
-    setSeacrhMenuToggle(true)
-  }
-  const searchModalTogleClose = () =>{
-    setSeacrhMenuToggle(false)
-  }
+  const getSearchInputValue = (e: any) => {
+    setSearchInputValue(e.target.value);
+
+    const filtredMovies = moviesBase.filter((item) => {
+      const searchValue = seacrhInputValue.toLowerCase();
+      return (
+        item.description.toLowerCase().includes(searchValue) ||
+        item.title.toLowerCase().includes(searchValue) ||
+        item.year.toString().includes(searchValue) ||
+        item.genre.forEach((genres) => genres.includes(searchValue))
+      );
+    });
+    setSeacrhetDatas(filtredMovies);
+  };
 
   return (
     <header>
@@ -40,22 +57,34 @@ const Header: React.FC = () => {
                   <input
                     className="input-seacrh"
                     placeholder="Search movies......."
-                    onFocus={()=> searchModalTogleOpen()}
-                    onBlur={()=>searchModalTogleClose()}
+                    onFocus={() => searchModalTogleOpen()}
+                    onBlur={() => searchModalTogleClose()}
+                    onChange={getSearchInputValue}
                   />
-                  {seacrhMenuToggle ? <div className="toggle-menu-seacrh">
-                    <div className="toggle-menu-seacrh-items">
-                      <div className="seacrh-menu-poster">
-                        <img className="poster-img-search" />
-                      </div>
-                      <div className="seacrh-menu-text-content">
-                        <p className="seacrh-menu-text-content-title">1</p>
-                        <p className="seacrh-menu-text-content-rank">2</p>
-                        <p className="seacrh-menu-text-content-rank-genre">3</p>
-                        <p className="seacrh-menu-text-content-rank-desc">3</p>
-                      </div>
+                  {seacrhMenuToggle ? (
+                    <div className="toggle-menu-seacrh">
+                      {seacrhetDatas.map((item) => (
+                        <div className="toggle-menu-seacrh-items">
+                          <div className="seacrh-menu-poster">
+                            <img
+                              className="poster-img-search"
+                              src={item.image}
+                            />
+                          </div>
+                          <div>
+                            <p className="poster-img-search-title">
+                              {item.title}
+                            </p>
+                            <p className="poster-img-search-title-desc">
+                              {item.description}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
                     </div>
-                  </div> : '' }
+                  ) : (
+                    ""
+                  )}
                 </div>
               </a>
             </li>
